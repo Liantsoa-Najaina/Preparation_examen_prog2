33fb684a0a442deda432ab5bff6db7a23dcea14a
Najaina <hei.najaina@gmail.com>
2024-07-24 15:38:36 +0300

    feat: ajout de Camion

---
+++ src/main/java/com/ferry/Camion.java
@@ -0,0 +1,9 @@
+package com.ferry;
+
+public class Camion extends Vehicule{
+    public Camion(String modele, String immatriculation, double kilometrage, double poidsVide, double longueur, Conducteur conducteur, double reserveCarburant) {
+        super(modele, immatriculation, kilometrage, poidsVide, longueur, conducteur, reserveCarburant);
+    }
+
+
+}
